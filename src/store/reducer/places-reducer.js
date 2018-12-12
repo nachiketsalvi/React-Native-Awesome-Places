f55f7b8759279d13d3a685448b41836853dc7ebf
Nachiket Salvi <nsalvi@mobiquityinc.com>
2018-12-12 15:01:57 +0530

    Created the folder structure for reducer, Created place reducer with default action

---
+++ src/store/reducer/places-reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
